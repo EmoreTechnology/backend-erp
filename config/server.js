476986dbc2a03b2fae79c30a8bb289e658a54028
emore23 <iurymarmore@gmail.com>
2022-02-15 19:41:55 -0300

    mais ajustes para heroku

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   url: env('MY_HEROKU_URL'),
 	proxy: true,
   app: {
-    keys: env.array("APP_KEYS", ["+WnnmSO+TKExz7Io6E0KTQ==", "tDKYmqgdX60xojxKxni+Og==", "da0UoZWnwgCAydwOYztIBQ==", "zv/JIU9ysyIVjT13kFiUAQ=="]),
+   	keys: env.array('APP_KEYS'),
   },
 });
  
